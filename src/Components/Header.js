8cfcb1f440adc4e1f50cb8ea2078fb7aab9066d3
Sujin <kgj1212_@naver.com>
2021-01-04 19:39:50 +0900

    [feat] withRouter

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from 'styled-components';
+// withRouter : router의 정보를 가져옴
 
 const Header = styled.header`
   position: fixed;
@@ -14,19 +15,17 @@ const Header = styled.header`
   color: white;
   background-color: rgba(20, 20, 20, 0.8);
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
-`
-
+`;
 const List = styled.ul`
   display: flex;
-`
+`;
 const Item = styled.li`
-  width: 50px;
+  width: 80px;
+  height: 50px
   text-align: center;
-
-  &:not(:last-child) {
-    margin-right: 10px
-  }
-`
+  border-bottom: 5px solid ${props => props.current ? "#3498db" : "transparent"};
+  transition: border-bottom 0.5s ease-in-out;
+`;
 const SLink = styled(Link)`
   display: flex;
   align-items: center;
@@ -34,18 +33,19 @@ const SLink = styled(Link)`
   height: 50px;
 `;
 
-export default () => (
+export default withRouter(({location: { pathname } }) => (
   <Header>
+    {/* {console.log(props)} */}
     <List>
-      <Item>
+      <Item current={pathname === "/"}>
         <SLink to="/">Movies</SLink>
       </Item>
-      <Item>
+      <Item current={pathname ==="/tv"}>
         <SLink to="/tv">TV</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/search"}>
         <SLink to="/search">Search</SLink>
       </Item>
     </List>
   </Header>
-)
+));
