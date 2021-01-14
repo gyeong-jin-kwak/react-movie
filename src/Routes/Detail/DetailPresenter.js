8027c65cc264d0c5fdcfb860b045159ef8c49707
곽경진 <kwakgyeongjin@gwaggyeongjin-ui-MacBookPro.local>
2021-01-14 11:46:10 +0900

    [feat] set up each Route presenter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,1 +1,17 @@
-export default () => "Detail";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Detail = ({ 
+    result, 
+    error, 
+    loading 
+}) => null;
+
+Detail.propTypes = {
+    result: PropTypes.object,
+    error: PropTypes.string,
+    loading: PropTypes.bool.isRequired
+}
+
+export default Detail;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,21 @@
-export default () => "Home";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Home = ({ 
+    nowPlaying, 
+    upcoming, 
+    popular, 
+    error, 
+    loading
+}) => null
+
+Home.propTypes = {
+    nowPlaying: PropTypes.array,
+    upcoming: PropTypes.array,
+    popular: PropTypes.array,
+    error: PropTypes.string,
+    loading: PropTypes.bool.isRequired
+}
+
+export default Home;
